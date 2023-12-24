5e722ad3dbb4bdc1dc20475ea0b66229cc95e0c7
brunoBrian <bruno.brian@vemprojuca.com>
2023-12-24 04:39:25 -0300

    favicon, loading, layout and header

---
+++ src/app/(store)/(home)/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+import { Header } from "@/app/components/header";
+
+export default function StoreLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+      <Header />
+      {children}
+    </div>
+  );
+}
