6ee23cdcbd151795f736378f78a5971ed8ef69d3
brunoBrian <bruno.brian@vemprojuca.com>
2023-12-28 03:01:19 -0300

    Cache & Memoization

--- src/app/services/products/index.ts
+++ src/app/services/products/index.ts
@@ -2,7 +2,11 @@ import { api } from "../api";
 import { Product } from "./types";
 
 export async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured");
+  const response = await api("/products/featured", {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
 
   const products = await response.json();
 
